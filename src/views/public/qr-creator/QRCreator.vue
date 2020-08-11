7edc0c4992ebf22124c12204d937f9419c4c3c0c
ductranvp <ductranvp@gmail.com>
2020-08-11 08:46:46 +0700

    update qr code page

--- src/views/public/qr-creator/QRCreator.vue
+++ src/views/public/qr-creator/QRCreator.vue
@@ -1,50 +1,105 @@
 <template>
-  <el-container class="full-size reset-container bg-success" direction="horizontal">
-    <el-aside width="50%">
-      <el-row class="full-size" type="flex" align="middle" justify="center">
-        <el-col :span="14">
-          <el-form ref="resetPasswordForm" :model="form">
-            <el-form-item>
-              <el-button @click="$router.go(-1)" class="full-width" type="info">Quay Lại</el-button>
-            </el-form-item>
-            <el-form-item prop="text">
-              <input-label effect="dark" label="Nội dung" required/>
-              <el-input type="textarea" rows="5" v-model="form.text"/>
-            </el-form-item>
-            <el-form-item prop="qrSize">
-              <input-label effect="dark" label="Kích thước" required/>
-              <el-input type="number" v-model="form.qrSize"/>
-            </el-form-item>
-            <el-form-item prop="qrMargin">
-              <input-label effect="dark" label="Canh Lề" required/>
-              <el-input type="number" v-model="form.qrMargin"/>
-            </el-form-item>
-          </el-form>
+  <el-container class="full-size bg-success" direction="vertical">
+    <el-header height="auto">
+      <el-row class="padding-0-20" type="flex" align="middle">
+        <el-col :span="8">
+          <el-button size="medium" type="success" @click="$router.go(-1)">
+            <i class="el-icon-arrow-left"></i>
+            <span>Quay lại</span>
+          </el-button>
         </el-col>
+        <el-col :span="8" class="text-center">
+          <h2 class="text-light">TẠO MÃ QR</h2>
+        </el-col>
+        <el-col :span="8"></el-col>
       </el-row>
-    </el-aside>
+    </el-header>
     <el-main>
-      <el-row class="full-size padding-20" type="flex" align="middle" justify="center">
-        <el-col :span="14">
-          <el-form>
-            <el-form-item v-if="form.text">
-              <div class="text-center">
-                <qrcode ref="qrcode" class="pointer" :value="form.text"
-                        :options="{ width: form.qrSize, margin: form.qrMargin }" tag="img"></qrcode>
-              </div>
-            </el-form-item>
-            <el-form-item>
-              <input-label effect="dark" label="Tên file tải xuống" optional/>
-              <el-input v-model="form.name"/>
-            </el-form-item>
-            <el-form-item>
-              <el-button @click="downloadImage()" class="full-width" type="warning">Tải xuống</el-button>
-            </el-form-item>
-          </el-form>
-        </el-col>
-      </el-row>
-    </el-main>
+      <el-container class="full-size" direction="horizontal">
+        <el-aside width="50%">
+          <el-row class="full-size" type="flex" align="top" justify="center">
+            <el-col :span="16">
+              <el-form ref="qrForm" :model="form">
+                <el-form-item prop="text">
+                  <input-label effect="dark" label="Nội dung" required/>
+                  <el-input type="textarea" rows="5" v-model="form.text" clearable/>
+                </el-form-item>
+                <el-form-item prop="qrSize">
+                  <input-label effect="dark" label="Kích thước"/>
+                  <el-input type="number" v-model="form.qrSize"/>
+                </el-form-item>
+                <el-form-item prop="qrMargin">
+                  <input-label effect="dark" label="Canh Lề"/>
+                  <el-input type="number" v-model="form.qrMargin"/>
+                </el-form-item>
+                <el-form-item>
+                  <el-row>
+                    <el-col :span="11">
+                      <el-form-item>
+                        <input-label effect="dark" label="Màu ma trận"/>
+                        <el-color-picker
+                          v-model="form.dark"
+                          color-format="hex"
+                          :predefine="predefineColors">
+                        </el-color-picker>
+                      </el-form-item>
+                    </el-col>
+                    <el-col :span="11" :offset="2">
+                      <el-form-item>
+                        <input-label effect="dark" label="Màu nền"/>
+                        <el-color-picker
+                          v-model="form.light"
+                          color-format="hex"
+                          :predefine="predefineColors">
+                        </el-color-picker>
+                      </el-form-item>
+                    </el-col>
+                  </el-row>
+                </el-form-item>
+              </el-form>
+            </el-col>
+          </el-row>
+        </el-aside>
+        <el-main>
+          <el-row class="full-size padding-20" type="flex" align="top" justify="center">
+            <el-col :span="16">
+              <el-form v-if="form.text">
+                <el-form-item>
+                  <div class="text-center">
+                    <qrcode ref="qrcode" class="pointer" :value="form.text"
+                            :options="{ logo: form.logo, width: form.qrSize, margin: form.qrMargin, color: {dark: form.dark, light: form.light} }"
+                            tag="img"></qrcode>
+                  </div>
+                </el-form-item>
+                <el-form-item>
+                  <el-row>
+                    <el-col :span="11">
+                      <el-form-item>
+                        <input-label effect="dark" label="Tên file"/>
+                        <el-input v-model="form.name" clearable/>
+                      </el-form-item>
+                    </el-col>
+                    <el-col :span="11" :offset="2">
+                      <el-form-item>
+                        <input-label effect="dark" label="Đuôi mở rộng"/>
+                        <el-input v-model="form.ext" clearable/>
+                      </el-form-item>
+                    </el-col>
+                  </el-row>
+                </el-form-item>
+                <el-form-item>
+                  <el-button @click="downloadImage()" class="full-width" type="warning">
+                    <i class="el-icon-download"></i>
+                    <span>Tải xuống</span>
+                  </el-button>
+                </el-form-item>
+              </el-form>
+            </el-col>
+          </el-row>
+        </el-main>
 
+      </el-container>
+    </el-main>
   </el-container>
 </template>
 
@@ -53,14 +108,26 @@
     name: "QRCreator",
     data() {
       return {
-        isLoading: false,
         form: {
+          dark: "#000000",
+          light: "#ffffff",
           text: "",
-          name: null,
           qrSize: 200,
-          qrMargin: 1
+          qrMargin: 1,
+          name: "qrcode",
+          ext: ".png"
         },
-        isSubmitted: false,
+        predefineColors: [
+          '#000000',
+          '#ffffff',
+          '#ff4500',
+          '#ff8c00',
+          '#ffd700',
+          '#90ee90',
+          '#00ced1',
+          '#1e90ff',
+          '#c71585'
+        ]
       };
     },
     methods: {
@@ -71,7 +138,7 @@
       downloadImage() {
         let link = document.createElement("a"); //Create <a>
         link.href = this.$refs.qrcode.$el.currentSrc; //Image Base64 Goes here
-        link.download = this.form.name ? this.form.name + ".png" : 'qrcode.png'; //File name Here
+        link.download = this.form.name ? this.form.name + this.form.ext : 'qrcode.png'; //File name Here
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
