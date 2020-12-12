208ef08649a6809272cece959ecae3f1fc51dbca
Duc Tran <ductranvp@gmail.com>
2020-12-12 15:49:15 +0700

    update ws

--- src/config/security/permission.js
+++ src/config/security/permission.js
@@ -4,7 +4,7 @@ import "@/assets/styles/nprogress.scss"; // progress bar style
 import AuthUtils from "@/utils/auth.util"; // get token from cookie
 import AppUtils from "@/utils/app.util";
 import store from "@/store";
-import StorageKey from '@/utils/storage-key';
+import StorageKey from "@/utils/storage-key";
 
 NProgress.configure({ showSpinner: true }); // NProgress Configuration
 
@@ -17,18 +17,19 @@ router.beforeEach(async (to, from, next) => {
 
   /* If no route matched => not found */
   if (!to.matched.length) {
-    next({name: "notFoundPage"});
+    next({ name: "notFoundPage" });
   }
 
   /* If need authorities */
   if (to.meta && to.meta.roles && to.meta.roles.length > 0) {
+    console.log("test");
     /* and have a token => check role */
     if (AuthUtils.getToken()) {
       /* If roles is not saved in store or page is refreshed
        * call api to get account info first then check roles,
        * else just check the roles */
-      if (!store.state.websocket.hasExecutedConnect){
-        store.dispatch("websocket/connectWS");
+      if (!store.state.websocket.hasExecutedConnect) {
+        await store.dispatch("websocket/connectWS");
       }
       if (!store.state.user.authorities.length) {
         try {
@@ -42,18 +43,20 @@ router.beforeEach(async (to, from, next) => {
             return;
           }
         }
-
       }
       /* If user has requested role */
       if (AuthUtils.hasAnyAuthority(to.meta.roles)) {
         next();
       } else {
-        next({name: "forbiddenPage"});
+        next({ name: "forbiddenPage" });
       }
     } else {
       /* and have no token => login */
-      sessionStorage.setItem(StorageKey.sessionStorageKeys.REQUESTED_URL, to.fullPath);
-      next({name: "loginPage"});
+      sessionStorage.setItem(
+        StorageKey.sessionStorageKeys.REQUESTED_URL,
+        to.fullPath
+      );
+      next({ name: "loginPage" });
     }
   } else {
     if (AuthUtils.getToken()) {
--- src/store/websocket/websocket.store.js
+++ src/store/websocket/websocket.store.js
@@ -3,13 +3,13 @@ import AuthUtils from "@/utils/auth.util";
 import * as SockJS from "sockjs-client";
 import * as Stomp from "webstomp-client";
 import Constants from "@/utils/constants";
-import {Notification} from "element-ui";
+import { Notification } from "element-ui";
 import NotificationUtils from "@/utils/notification.util";
 import WebsocketConstants from "@/utils/websocket.constants";
 import CloudFlareService from "@/service/shared/cloudflare.service";
 import AppUtils from "@/utils/app.util";
-import WebsocketEndpoints from '@/utils/websocket.endpoints';
-import router from '@/router';
+import WebsocketEndpoints from "@/utils/websocket.endpoints";
+import router from "@/router";
 
 const state = {
   wsError: null,
@@ -20,10 +20,10 @@ const state = {
   retryCount: 0
 };
 const mutations = {
-  SET_RETRY_COUNT(state, num){
+  SET_RETRY_COUNT(state, num) {
     state.retryCount = num;
   },
-  INCREASE_RETRY(state){
+  INCREASE_RETRY(state) {
     state.retryCount++;
   },
   SET_HAS_EXECUTED_CONNECT(state, status) {
@@ -47,7 +47,8 @@ const mutations = {
     if (!state.wsError) {
       state.wsError = Notification.error({
         title: "Lỗi kết nối tới máy chủ!",
-        message: "<span>Tải lại trang hoặc kiểm tra các kết nối mạng!<br>Đang thực hiện kết nối lại..</span>",
+        message:
+          "<span>Tải lại trang hoặc kiểm tra các kết nối mạng!<br>Đang thực hiện kết nối lại..</span>",
         dangerouslyUseHTMLString: true,
         showClose: false,
         duration: 0
@@ -58,38 +59,54 @@ const mutations = {
     if (state.wsError) {
       state.wsError.close();
       state.wsError = null;
-      setTimeout(() => NotificationUtils.success("Kết nối lại thành công, tải lại trang để đảm bảo dữ liệu mới nhất"), 300);
+      setTimeout(
+        () =>
+          NotificationUtils.success(
+            "Kết nối lại thành công, tải lại trang để đảm bảo dữ liệu mới nhất"
+          ),
+        300
+      );
     }
-  },
+  }
 };
 const actions = {
-  connectWS({state, commit, dispatch}) {
+  connectWS({ state, commit, dispatch }) {
     /* WS will automatic reconnect when disconnected, so this function must be called once */
+    if (!AuthUtils.getToken()) {
+      dispatch("disconnectWS");
+      return;
+    }
+
     commit("SET_HAS_EXECUTED_CONNECT", true);
 
-    let url = Constants.SERVER_API_URL + "/websocket?access_token=" + AuthUtils.getToken();
-    let options = {debug: false, protocols: ['v12.stomp']};
+    let url =
+      Constants.SERVER_API_URL +
+      "/websocket?access_token=" +
+      AuthUtils.getToken();
+    let options = { debug: false, protocols: ["v12.stomp"] };
     let socket = new SockJS(url);
     let stompClient = Stomp.over(socket, options);
     stompClient.connect(
       {},
-      function (frame) {
+      function(frame) {
         commit("SET_CONNECTED", true);
         commit("SET_STOMP_CLIENT", stompClient);
         commit("CLEAR_ERROR");
         commit("SET_RETRY_COUNT", 0);
         dispatch("sendActivity", {});
       },
-      function (error) {
+      function(error) {
         commit("SET_CONNECTED", false);
         commit("SET_STOMP_CLIENT", null);
         commit("SET_ERROR");
         setTimeout(() => {
           commit("INCREASE_RETRY");
-          if (state.retryCount < state.maxRetry){
+          if (state.retryCount < state.maxRetry) {
             dispatch("connectWS");
           } else {
-            alert("Không thể tự kết nối lại với máy chủ, trang web sẽ được tải lại.");
+            alert(
+              "Không thể tự kết nối lại với máy chủ, trang web sẽ được tải lại."
+            );
             location.reload();
           }
         }, WebsocketConstants.RECONNECT_DELAY);
@@ -97,24 +114,25 @@ const actions = {
     );
   },
   // use when logout only
-  disconnectWS({state, commit}) {
+  disconnectWS({ state, commit }) {
     if (state.wsStompClient) {
       state.wsStompClient.disconnect();
-      commit("CLEAR_ERROR");
-      commit("SET_CONNECTED", false);
-      commit("SET_STOMP_CLIENT", null);
-      commit("SET_HAS_EXECUTED_CONNECT", false);
     }
+    commit("CLEAR_ERROR");
+    commit("SET_CONNECTED", false);
+    commit("SET_STOMP_CLIENT", null);
+    commit("SET_HAS_EXECUTED_CONNECT", false);
   },
-  async sendActivity({state}, activity) {
+  async sendActivity({ state }, activity) {
     activity.cloudFlareTrace = {};
     try {
-      const {data} = await CloudFlareService.getCouldFlareTrace();
+      const { data } = await CloudFlareService.getCouldFlareTrace();
       activity.cloudFlareTrace = AppUtils.parseCloudFlareTrace(data);
     } catch (e) {
       // error get cloudFlare trace
     }
-    state.wsStompClient.send(WebsocketEndpoints.APP_ACTIVITY, // destination
+    state.wsStompClient.send(
+      WebsocketEndpoints.APP_ACTIVITY, // destination
       JSON.stringify(activity), // body
       {} // header
     );
